e17745053a6b2795569e04f62d6d4b9c0e05c1f4
Manjunath <121750296+Manju200417@users.noreply.github.com>
2025-07-28 17:06:44 +0530

    Add files via upload

---
+++ Calculator_Server.py
@@ -0,0 +1,27 @@
+# Calculator Server(REST API)
+
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST'])
+def operation():
+    res = request.get_json()
+
+    op = res['op']
+    a = float(res['first'])
+    b = float(res['second'])
+
+    match op:
+        case "add": ans = f"Addition of two numbers: {a + b}"
+        case "sub": ans = f"Subtraction of two numbers: {a - b}"
+        case "mul": ans = f"Multiplication of two numbers: {a * b}"
+        case "div":
+            if b == 0: return jsonify({"error": "Cannot divide by zero!"})
+            ans = f"Division of two numbers: {a / b}"
+        case _: ans = "Invalid Operator!!"
+
+    return jsonify({"Result": ans})
+
+if __name__ == '__main__':
+    app.run(port=8080, debug=False)
